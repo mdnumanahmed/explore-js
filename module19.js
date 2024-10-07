8e469643769a6c3e68fda2b8446bbd633343ca8b
mdnumanahmed <mnumansyl@gmail.com>
2024-10-07 21:27:17 +0600

    Start Object practice tasks

---
+++ module19.js
@@ -0,0 +1,97 @@
+/* ====================================
+                OBJECT
+=======================================*/
+
+{
+    // Task-1 : Access the golden rod color value in output.
+    const colors = {
+        red: "#ff0000",
+        green: "#00ff00",
+        blue: "#0000ff",
+        "golden rod": '#daa520'
+    };
+}
+
+{
+    // Task-2:  For this object below add a property named passenger capacity with value 5
+    const car = {
+        make: "Toyota",
+        model: "Corolla",
+        year: 2020
+    };
+}
+
+{
+    // Task-3:  Display the physics marks as output.
+    const student = {
+        name: "Hamim Sakep",
+        id: 5421,
+        physics: {
+            subject: "HSC Physics",
+            author: "Shahjahan Tapan",
+            marks: 30
+        }
+    };
+}
+
+{
+    // Task-4: Count the number of properties.
+    let student = {
+        name: 'Ariana Grande',
+        age: 21,
+        city: 'Gaibandha',
+        isStudent: true
+    };
+}
+
+{
+    // Task-5 (Hard) Loop through an object and print the key-value pairs with their types
+    Input:
+
+    let myObject = {
+        name: 'John Doe',
+        age: 25,
+        city: 'Example City',
+        isStudent: true
+    };
+
+/* Output:
+
+key: name | type:  string
+key: age | type:  number
+key: city | type:  string
+key: isStudent | type:  boolean */
+}
+
+/* ====================================
+               STRING
+=======================================*/
+
+/* 
+Task-1:
+Count how many times a string has the letter a
+ */
+
+/* 
+Task-2:
+Count how many times a string has the letter a or A
+ */
+
+/* 
+Task-3:
+Check whether a string contains all the vowels a, e, i, o, u
+ */
+
+/* 
+Task-4:
+If a given string has either x, replace x by y. if the given string has X, replace it by Y.
+
+Hint: You should be able to check whether x or X exists. After that, search online how to replace a character in a string.
+ */
+
+
+/* 
+Task-5:
+Capitalize Every first Letter of each word in a String
+ */
+
