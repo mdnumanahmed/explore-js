ffc1ad75d38a9e7ac2f5f8019f7d78cae7ba9131
mdnumanahmed <mnumansyl@gmail.com>
2024-10-08 22:07:21 +0600

    Task two done

--- module20.js
+++ module20.js
@@ -14,7 +14,17 @@ Take four parameters. Multiply the four numbers and then return the result
 Task-2
 Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
 */
-
+{
+  function oddEvenOperation(number) {
+    if (number % 2 === 1) {
+      return number * 2;
+    } else {
+      return number / 2;
+    }
+  }
+  const result = oddEvenOperation(4);
+  console.log(result);
+}
 /* 
 Task-3
 Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
