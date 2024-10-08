9ad6ec605b1f141452b7bae0a6568fa8e0ab4a0b
mdnumanahmed <mnumansyl@gmail.com>
2024-10-08 22:53:45 +0600

    Task four done

--- module20.js
+++ module20.js
@@ -29,12 +29,36 @@ Take a number if the number is odd multiply it by 2 and return the result. If th
 Task-3
 Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
 */
-
+{
+  const numbers = [5, 10, 15, 30];
+  function makeAvg(numbers, size) {
+    let total = 0;
+    for (const number of numbers) {
+      total = total + number;
+    }
+    return total / size;
+  }
+  const avg = makeAvg(numbers, numbers.length);
+  console.log(avg);
+}
 /* 
 Task-4
 Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
 */
-
+{
+  const binaryString = "1010101001010101";
+  function countZero(bStr) {
+    let count = 0;
+    for (const zero of bStr) {
+      if (zero === "0") {
+        count++;
+      }
+    }
+    return count;
+  }
+  const result = countZero(binaryString);
+  console.log(result);
+}
 /* 
 Task-5
 Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
