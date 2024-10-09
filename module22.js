b69b1c61be9b2d2e5bf13799f61c3874bed4ca4d
mdnumanahmed <mnumansyl@gmail.com>
2024-10-09 23:08:46 +0600

    Task four done

--- module22.js
+++ module22.js
@@ -91,6 +91,26 @@ Input
         { model: "PhoneF", brand: "HTC", price: 48000 },
     ];
 */
+{
+  const phones = [
+    { model: "PhoneA", brand: "Iphone", price: 95000 },
+    { model: "PhoneB", brand: "Samsung", price: 40000 },
+    { model: "PhoneC", brand: "Oppo", price: 26000 },
+    { model: "PhoneD", brand: "Nokia", price: 35000 },
+    { model: "PhoneE", brand: "Iphone", price: 105000 },
+    { model: "PhoneF", brand: "HTC", price: 48000 },
+  ];
+  function findAveragePhonePrice(phones) {
+    let totalPrice = 0;
+    for (const phone of phones) {
+      totalPrice += phone.price;
+    }
+    const average = (totalPrice / phones.length).toFixed(2);
+    return average;
+  }
+  const average = findAveragePhonePrice(phones);
+  console.log(+average);
+}
 
 /*
 Task -5: (Hard)
