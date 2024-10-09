d486584fd81e83892aec24d94d337620c68af11d
mdnumanahmed <mnumansyl@gmail.com>
2024-10-09 23:27:52 +0600

    Task Five completed

--- module22.js
+++ module22.js
@@ -123,3 +123,25 @@ For each employee their current salary is calculated by multiplying yearly incre
             { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
         ];
 */
+{
+  const employees = [
+    { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+    { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+    { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+    { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+  ];
+  function totalSalary(employees) {
+    let salary = 0;
+    for (const employee of employees) {
+      salary += employee.increment * employee.experience + employee.starting;
+      // or
+      // let increment = employee.increment * employee.experience;
+      //   let currentSalary = increment + employee.starting;
+      //   salary += currentSalary;
+      console.log(salary);
+    }
+    return salary;
+  }
+  const total = totalSalary(employees);
+  console.log(total);
+}
