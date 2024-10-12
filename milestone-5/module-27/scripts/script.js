2ac1f10e6f55501a5aa229bbd2809248732ab21a
mdnumanahmed <mnumansyl@gmail.com>
2024-10-13 02:06:11 +0600

    added score and life update functionality

--- milestone-5/module-27/scripts/script.js
+++ milestone-5/module-27/scripts/script.js
@@ -1,6 +1,36 @@
+function handleKeyUpEvent(event) {
+  const playerPressed = event.key;
+
+  const currentLetter = document.getElementById("current-alphabet").innerText;
+  const expectedLetter = currentLetter.toLowerCase();
+
+  if (playerPressed.toLowerCase() === expectedLetter.toLowerCase()) {
+    const currentScoreElement = document.getElementById("current-score");
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    const newScore = currentScore + 1;
+    currentScoreElement.innerText = newScore;
+
+    removeBackgroundColorById(expectedLetter);
+    continueGame();
+  } else {
+    const currentLifeElement = document.getElementById("current-life");
+    const currentLifeText = currentLifeElement.innerText;
+    const currentLife = parseInt(currentLifeText);
+    const newLife = currentLife - 1;
+    currentLifeElement.innerText = newLife;
+  }
+}
+
+document.addEventListener("keyup", handleKeyUpEvent);
+
 function continueGame() {
-  const letterDisplay = document.getElementById("random-letter");
-  letterDisplay.innerText = getRandomLetter();
+  const alphabet = getRandomLetter();
+
+  const letterDisplay = document.getElementById("current-alphabet");
+  letterDisplay.innerText = alphabet;
+
+  setBackgroundColorById(alphabet);
 }
 
 function play() {
--- milestone-5/module-27/scripts/utilities.js
+++ milestone-5/module-27/scripts/utilities.js
@@ -1,11 +1,21 @@
 function hideElementById(elementId) {
-  const homeScreen = document.getElementById(elementId);
-  homeScreen.classList.add("hidden");
+  const element = document.getElementById(elementId);
+  element.classList.add("hidden");
 }
 
 function showElementById(elementId) {
-  const playGround = document.getElementById(elementId);
-  playGround.classList.remove("hidden");
+  const element = document.getElementById(elementId);
+  element.classList.remove("hidden");
+}
+
+function setBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bg-orange-400");
+}
+
+function removeBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-orange-400");
 }
 
 function getRandomLetter() {
