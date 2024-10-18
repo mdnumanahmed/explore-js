97c64cd62a158f51354de542ad6a0d491b4b3e73
mdnumanahmed <mnumansyl@gmail.com>
2024-10-18 17:26:33 +0600

    Problem Three solved

--- milestone-6/module30.js
+++ milestone-6/module30.js
@@ -30,3 +30,18 @@
   const result = findEvenLengthFriends(friends);
   console.log(result);
 }
+
+// Problem Three
+{
+  const numbers = [2, 3, 4, 5, 6];
+  const squareIt = (numbers) => {
+    let sum = 0;
+    for (const number of numbers) {
+      sum += number * number;
+    }
+    const average = sum / numbers.length;
+    return average;
+  };
+  const result = squareIt(numbers);
+  console.log(result);
+}
