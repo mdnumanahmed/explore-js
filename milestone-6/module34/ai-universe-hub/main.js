2fae2b378e3d4bf9c1a6d8fa0fe2dfb8775955ef
mdnumanahmed <mnumansyl@gmail.com>
2024-10-21 22:41:05 +0600

    added sort by date funtionality

--- milestone-6/module34/ai-universe-hub/main.js
+++ milestone-6/module34/ai-universe-hub/main.js
@@ -1,14 +1,18 @@
+// Global variable to store loaded data
+let loadedTools;
 // Funtion to Load all AI Tools from api
 const loadTools = async () => {
   const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
   const data = await res.json();
   const tools = data.data.tools;
+  loadedTools = data.data.tools;
   displayTools(tools);
 };
 
 // Function to display data in the UI
 const displayTools = (tools) => {
   const toolsContainer = document.getElementById("tools-container");
+  toolsContainer.textContent = "";
   tools.forEach((tool) => {
     // console.log(tool.features);
     const { image, features, name, published_in } = tool;
@@ -47,6 +51,14 @@ const displayTools = (tools) => {
       `;
     toolsContainer.appendChild(toolCard);
   });
+};
+
+// Fucntion to sort by date
+const sortByDate = () => {
+  const sortData = loadedTools.sort(function (a, b) {
+    return new Date(a.published_in) - new Date(b.published_in);
+  });
+  displayTools(sortData);
 };
 
 loadTools();
