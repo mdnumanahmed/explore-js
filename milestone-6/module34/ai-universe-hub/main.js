7b53c51c6cc8b44523ff623075f7d2286332e226
mdnumanahmed <mnumansyl@gmail.com>
2024-10-21 21:46:02 +0600

    Loaded data form API and displayed in the UI Dynamically

---
+++ milestone-6/module34/ai-universe-hub/main.js
@@ -0,0 +1,52 @@
+// Funtion to Load all AI Tools from api
+const loadTools = async () => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
+  const data = await res.json();
+  const tools = data.data.tools;
+  displayTools(tools);
+};
+
+// Function to display data in the UI
+const displayTools = (tools) => {
+  const toolsContainer = document.getElementById("tools-container");
+  tools.forEach((tool) => {
+    // console.log(tool.features);
+    const { image, features, name, published_in } = tool;
+    const toolCard = document.createElement("div");
+    toolCard.classList = `w-96 bg-slate-50 p-8 rounded-xl shadow-lg space-y-5`;
+    toolCard.innerHTML = `
+      <div>
+        <img src="${
+          image
+            ? image
+            : "https://image-placeholder.com/images/actual-size/1920x1080.png"
+        }" alt="" />
+        <h4 class="text-2xl font-semibold work-sans mt-5">Features</h4>
+         <ul class="list-outside list-decimal pl-5">
+         ${features.map((feature) => `<li>${feature}</li>`).join("")}
+         </ul>
+        </div>
+        <hr />
+        <div class="flex justify-between items-center">
+        <div>
+            <h4 class="text-2xl font-semibold work-sans">${
+              name ? name : "No Name Found"
+            }</h4>
+            <p><span class="mr-3">📆</span>${
+              published_in ? published_in : "No Date Found"
+            }</p>
+        </div>
+        <div>
+            <button
+            class="bg-[#f3dbdb] text-red-600 rounded-full w-12 h-12 pb-1 text-3xl hover:bg-red-400 hover:text-white"
+            >
+            →
+            </button>
+        </div>
+        </div>
+      `;
+    toolsContainer.appendChild(toolCard);
+  });
+};
+
+loadTools();
