25d38f4ad8237dde60d463afda746933aaf0a498
mdnumanahmed <mnumansyl@gmail.com>
2024-10-22 01:41:40 +0600

    Designed Modal UI and loaded some data

--- milestone-6/module34/ai-universe-hub/main.js
+++ milestone-6/module34/ai-universe-hub/main.js
@@ -23,7 +23,7 @@ const displayTools = (tools, dataLimit) => {
   }
   tools.forEach((tool) => {
     // console.log(tool.features);
-    const { image, features, name, published_in } = tool;
+    const { id, image, features, name, published_in } = tool;
     const toolCard = document.createElement("div");
     toolCard.classList = `w-96 bg-slate-50 p-8 rounded-xl shadow-lg space-y-5`;
     toolCard.innerHTML = `
@@ -50,6 +50,7 @@ const displayTools = (tools, dataLimit) => {
         </div>
         <div>
             <button
+            onclick="loadToolDetails('${id}')"
             class="bg-[#f3dbdb] text-red-600 rounded-full w-12 h-12 pb-1 text-3xl hover:bg-red-400 hover:text-white"
             >
             →
@@ -69,6 +70,95 @@ const sortByDate = () => {
   displayTools(sortData);
 };
 
+// Load Specific tool by id
+const loadToolDetails = async (id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/ai/tool/${id}`
+  );
+  const data = await res.json();
+  const tool = data.data;
+  showToolDetails(tool);
+  show_details_modal.showModal();
+};
+
+const showToolDetails = (tool) => {
+  console.log(tool);
+  const {
+    image_link,
+    pricing,
+    features,
+    input_output_examples,
+    description,
+    accuracy,
+    integrations,
+  } = tool;
+  const showDetailModal = document.getElementById("show_details_modal");
+  showDetailModal.innerHTML = `
+  <div class="modal-box !max-w-screen-lg overflow-y-visible">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-5">
+      <div class="bg-pink-50 rounded-xl shadow-lg">
+        <h3 class="text-lg font-bold">
+          ${description}
+        </h3>
+        <div class="flex gap-6">
+          <p
+            class="text-lg font-semibold bg-white p-5 rounded-lg text-green-600"
+          >
+            $10/month Basic
+          </p>
+          <p
+            class="text-lg font-semibold bg-white p-5 rounded-lg text-orange-600"
+          >
+            $50/month Pro
+          </p>
+          <p
+            class="text-lg font-semibold bg-white p-5 rounded-lg text-red-600"
+          >
+            Contact us Enterprise
+          </p>
+        </div>
+        <div class="grid grid-cols-2 gap-5">
+          <div>
+            <h3 class="text-2xl font-semibold work-snas my-4">Features</h3>
+            <ul>
+              <li>Customizable responses</li>
+              <li>Multilingual support</li>
+              <li>Seamless integration</li>
+            </ul>
+          </div>
+          <div>
+            <h3 class="text-2xl font-semibold work-snas my-4">Integrations</h3>
+            <ul>
+              <li>Customizable responses</li>
+              <li>Multilingual support</li>
+              <li>Seamless integration</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="bg-pink-50 rounded-xl shadow-lg">
+        <img src="${image_link[0]}" alt="" />
+        <h3 class="text-lg font-bold">${input_output_examples[0]?.input}</h3>
+        <p class="py-4">
+         ${input_output_examples[0]?.output}
+        </p>
+      </div>
+    </div>
+    <div class="modal-action absolute -top-10 -right-4">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button
+          class="text-white w-10 h-10 bg-red-500 rounded-full z-[9999]"
+        >
+          X
+        </button>
+      </form>
+    </div>
+  </div>
+  `;
+};
+
+// Show All Tools Functionality by clicking on Show More Button
 showMoreBtn.addEventListener("click", function () {
   loadTools(false);
 });
